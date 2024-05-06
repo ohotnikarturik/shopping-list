081998a2ce4e850688bd4ad9fa004e969c24f0b5
Artur <ohotnikarturik@gmail.com>
2024-05-06 10:28:58 +0300

    feat: extend Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,38 @@
 import { Outlet } from "react-router-dom"
-import Navigation from "./Navigation"
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
+
+import AppBar from "./AppBar"
+import Sidebar from "./Sidebar"
+
+const SIDEBAR_OPEN = "SHOPPING_LIST_SIDEBAR_OPEN"
+
+const getStoredSidebarValue = () => {
+  const storedValue = localStorage.getItem(SIDEBAR_OPEN)
+  if (!storedValue) return true
+  return JSON.parse(storedValue)
+}
 
 const Layout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(getStoredSidebarValue)
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+    localStorage.setItem(SIDEBAR_OPEN, JSON.stringify(!isSidebarOpen))
+  }
+
   return (
-    <>
-      <header>
-        <h1>Shopping List app</h1>
-        <Navigation />
-      </header>
-      <main>
-        <Suspense fallback={<div>Page is Loading...</div>}>
-          <Outlet />
-        </Suspense>
-      </main>
-      <footer>Footer</footer>
-    </>
+    <div className="flex h-screen divide-x">
+      <Sidebar open={isSidebarOpen} />
+      <div className="w-full flex flex-col divide-y">
+        <AppBar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+        <main className="flex-1 p-4 overflow-auto">
+          <Suspense fallback={<div>Page is Loading...</div>}>
+            <Outlet />
+          </Suspense>
+        </main>
+        <footer>Footer</footer>
+      </div>
+    </div>
   )
 }
 export default Layout
